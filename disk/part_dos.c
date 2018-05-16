0c290ead961e67d95ac57fc9a895878620043aca
German Tretyakov <gt@nov-nil01>
2018-05-16 14:39:02 +0300

    Workaround problem with ACMD13 and ACM51 in SD card driver + fix in part_dos

--- disk/part_dos.c
+++ disk/part_dos.c
@@ -90,7 +90,8 @@ static int test_block_type(unsigned char *buffer)
 static int part_test_dos(struct blk_desc *dev_desc)
 {
 #ifndef CONFIG_SPL_BUILD
-	ALLOC_CACHE_ALIGN_BUFFER(legacy_mbr, mbr, dev_desc->blksz);
+	//ALLOC_CACHE_ALIGN_BUFFER(legacy_mbr, mbr, dev_desc->blksz);  //!!! wrong size
+	ALLOC_CACHE_ALIGN_BUFFER_PAD(legacy_mbr, mbr, 1, dev_desc->blksz);
 
 	if (blk_dread(dev_desc, 0, 1, (ulong *)mbr) != 1)
 		return -1;
