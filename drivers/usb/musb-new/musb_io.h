78f08bc879a5d33c3fe61f283208055b350a6a13
root <root@redd.local>
2020-02-27 16:01:47 +0300

    Unnesesary spases were removed

--- drivers/usb/musb-new/musb_io.h
+++ drivers/usb/musb-new/musb_io.h
@@ -24,7 +24,6 @@
 	&& !defined(CONFIG_PPC32) \
 	&& !defined(CONFIG_PPC64) && !defined(CONFIG_MIPS) \
 	&& !defined(CONFIG_M68K)
-
 static inline void readsl(const void __iomem *addr, void *buf, int len)
 	{ insl((unsigned long)addr, buf, len); }
 static inline void readsw(const void __iomem *addr, void *buf, int len)
@@ -44,12 +43,12 @@ static inline void writesb(const void __iomem *addr, const void *buf, int len)
 /* NOTE:  these offsets are all in bytes */
 
 #ifndef __BIG_ENDIAN
-
 static inline u16 musb_readw(const void __iomem *addr, unsigned offset)
 	{ return __raw_readw(addr + offset); }
 
 static inline u32 musb_readl(const void __iomem *addr, unsigned offset)
 	{ return __raw_readl(addr + offset); }
+
 
 static inline void musb_writew(void __iomem *addr, unsigned offset, u16 data)
 	{ __raw_writew(data, addr + offset); }
