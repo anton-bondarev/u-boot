f6f5a817674e36c997be1641455926c355205201
Vladimir.Shalyt <Vladimir.Shalyt@astrosoft.ru>
2020-09-17 12:05:53 +0300

    volatile buffers for mt150-02

--- board/rcm/common/flw_buffers.c
+++ board/rcm/common/flw_buffers.c
@@ -8,9 +8,9 @@ volatile char* edcl_xmodem_buf_sync;
 
 #ifdef CONFIG_TARGET_1888BM18
 #define IM1_BASE_ADDRESS 0xC00000000
-char* edcl_xmodem_buf0 = (char*)IM1_BASE_ADDRESS;
-char* edcl_xmodem_buf1 = (char*)(IM1_BASE_ADDRESS + EDCL_XMODEM_BUF_LEN);
-char* edcl_xmodem_buf_sync = (char*)(IM1_BASE_ADDRESS + 2*EDCL_XMODEM_BUF_LEN);
+volatile char* edcl_xmodem_buf0 = (volatile char*)IM1_BASE_ADDRESS;
+volatile char* edcl_xmodem_buf1 = (volatile char*)(IM1_BASE_ADDRESS + EDCL_XMODEM_BUF_LEN);
+volatile char* edcl_xmodem_buf_sync = (volatile char*)(IM1_BASE_ADDRESS + 2*EDCL_XMODEM_BUF_LEN);
 char* blk_buf = (char*)(IM1_BASE_ADDRESS + 2*EDCL_XMODEM_BUF_LEN+sizeof(char*));
 #endif
 
