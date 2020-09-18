7f4f889a152944febf7bc937dcdb9ceff253d1b3
Vladimir.Shalyt <Vladimir.Shalyt@astrosoft.ru>
2020-09-18 15:55:59 +0300

    and corrections for mt150

--- board/rcm/common/flw_buffers.c
+++ board/rcm/common/flw_buffers.c
@@ -1,18 +1,16 @@
 #include "flw_buffers.h"
 
-#ifdef CONFIG_TARGET_1888TX018
-volatile char blk_buf[512] ;
 volatile char edcl_xmodem_buf0[EDCL_XMODEM_BUF_LEN] __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
 volatile char edcl_xmodem_buf1[EDCL_XMODEM_BUF_LEN] __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
 volatile char* edcl_xmodem_buf_sync __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
+
+#ifdef CONFIG_TARGET_1888TX018
+volatile char blk_buf[512] ;
 #endif
 
 #ifdef CONFIG_TARGET_1888BM18
-#define IM1_BASE_ADDRESS 0xC00000000
+#define IM1_BASE_ADDRESS 0xC0000000
 volatile char* blk_buf = (volatile char*)(IM1_BASE_ADDRESS);
-volatile char* edcl_xmodem_buf0 = (volatile char*)(IM1_BASE_ADDRESS + 512);
-volatile char* edcl_xmodem_buf1 = (volatile char*)(IM1_BASE_ADDRESS + 512 + EDCL_XMODEM_BUF_LEN);
-volatile char* edcl_xmodem_buf_sync = (volatile char*)(IM1_BASE_ADDRESS + 512 + 2*EDCL_XMODEM_BUF_LEN);
 #endif
 
 uint32_t flw_virt_to_dma(volatile void* ptr)
--- board/rcm/common/flw_buffers.h
+++ board/rcm/common/flw_buffers.h
@@ -6,7 +6,14 @@
 #define EDCL_XMODEM_BUF_LEN 4096
 #define EDCL_XMODEM_BUF_ALIGN 64
 
+#ifdef CONFIG_TARGET_1888TX018
 extern volatile char blk_buf[];
+#endif
+
+#ifdef CONFIG_TARGET_1888BM18
+extern volatile char* blk_buf;
+#endif
+
 extern volatile char edcl_xmodem_buf0[];
 extern volatile char edcl_xmodem_buf1[];
 extern volatile char* edcl_xmodem_buf_sync;
