439df5403ac5d2bcd0135f2cf8d95644a8f5a995
Vladimir.Shalyt <Vladimir.Shalyt@astrosoft.ru>
2020-10-02 13:01:49 +0300

    Baudrate setup

--- board/rcm/common/flw_serial.h
+++ board/rcm/common/flw_serial.h
@@ -2,14 +2,20 @@
 #define FLW_SERIAL_H
 
 #ifdef CONFIG_TARGET_1888TX018
+    #define BAUDRATE_MIN 2400
+    #define BAUDRATE_MAX 1000000
     #define  FLW_TOUT 1000000
 #endif // empirical,can be changed
 
 #ifdef CONFIG_TARGET_1888BM18
+    #define BAUDRATE_MIN 2400
+    #define BAUDRATE_MAX 6000000
     #define  FLW_TOUT 1000
 #endif // too
 
 #ifdef CONFIG_TARGET_1888BC048
+    #define BAUDRATE_MIN 2400
+    #define BAUDRATE_MAX 115200
     #define  FLW_TOUT 100000
 #endif // too
 
