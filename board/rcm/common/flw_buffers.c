0cb75a91a540ce2eeee3f6bc533657e9cda0dac0
Andrew Andrianov <andrew@ncrmnt.org>
2021-08-12 14:23:37 +0300

    flw_buffers: Adjust buffers for 1888TX018
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- board/rcm/common/flw_buffers.c
+++ board/rcm/common/flw_buffers.c
@@ -1,13 +1,21 @@
 #include "flw_buffers.h"
 
+
 #ifdef CONFIG_TARGET_1888TX018
+#ifdef USE_DSP_MEM
+#define IM1_BASE_ADDRESS 0x100000
+volatile char* blk_buf = (volatile char*)(IM1_BASE_ADDRESS);
+volatile char* edcl_xmodem_buf0 = (volatile char*)(IM1_BASE_ADDRESS + EDCL_XMODEM_BUF_LEN);
+volatile char* edcl_xmodem_buf1 = (volatile char*)(IM1_BASE_ADDRESS + EDCL_XMODEM_BUF_LEN * 2);
+volatile char** p_edcl_xmodem_buf_sync = (volatile char**)(IM1_BASE_ADDRESS + EDCL_XMODEM_BUF_LEN * 3);
+volatile char** p_edcl_xmodem_buf_sync_ack = (volatile char**)(IM1_BASE_ADDRESS + EDCL_XMODEM_BUF_LEN * 3 + 0x100);
+#else
 volatile char blk_buf[512];
 volatile char edcl_xmodem_buf0[EDCL_XMODEM_BUF_LEN] __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
 volatile char edcl_xmodem_buf1[EDCL_XMODEM_BUF_LEN] __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
 volatile char* edcl_xmodem_buf_sync __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
-#ifdef CONFIRM_HANDSHAKE
-    volatile char* edcl_xmodem_sync_conf __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
-#endif // CONFIRM_HANDSHAKE
+volatile char* edcl_xmodem_buf_sync_ack __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
+#endif
 #endif // CONFIG_TARGET_1888TX018
 
 #ifdef CONFIG_TARGET_1888BM18
@@ -16,9 +24,7 @@ volatile char* blk_buf = (volatile char*)(IM1_BASE_ADDRESS);
 volatile char* edcl_xmodem_buf0 = (volatile char*)(IM1_BASE_ADDRESS+0x2000);
 volatile char* edcl_xmodem_buf1 = (volatile char*)(IM1_BASE_ADDRESS+0x4000);
 volatile char** p_edcl_xmodem_buf_sync = (volatile char**)(IM1_BASE_ADDRESS+0x1000);
-#ifdef CONFIRM_HANDSHAKE
-    volatile char** p_edcl_xmodem_sync_conf = (volatile char**)(IM1_BASE_ADDRESS+0x1004);
-#endif // CONFIRM_HANDSHAKE
+volatile char** p_edcl_xmodem_buf_sync_ack = (volatile char**)(IM1_BASE_ADDRESS+0x1000);
 #endif // CONFIG_TARGET_1888BM18
 
 #ifdef CONFIG_TARGET_1888BC048
@@ -28,9 +34,7 @@ volatile char newout[EDCL_XMODEM_BUF_LEN+64];
 volatile char edcl_xmodem_buf0[EDCL_XMODEM_BUF_LEN] __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
 volatile char edcl_xmodem_buf1[EDCL_XMODEM_BUF_LEN] __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
 volatile char* edcl_xmodem_buf_sync __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
-#ifdef CONFIRM_HANDSHAKE
-    volatile char* edcl_xmodem_sync_conf __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
-#endif // CONFIRM_HANDSHAKE
+volatile char* edcl_xmodem_buf_sync_ack __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
 #endif // CONFIG_TARGET_1888BC048
 
 #ifdef CONFIG_TARGET_1879VM8YA
@@ -40,12 +44,14 @@ volatile char newout[EDCL_XMODEM_BUF_LEN+64];
 volatile char edcl_xmodem_buf0[EDCL_XMODEM_BUF_LEN] __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
 volatile char edcl_xmodem_buf1[EDCL_XMODEM_BUF_LEN] __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
 volatile char* edcl_xmodem_buf_sync __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
-#ifdef CONFIRM_HANDSHAKE
-    volatile char* edcl_xmodem_sync_conf __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
-#endif // CONFIRM_HANDSHAKE
+volatile char* edcl_xmodem_buf_sync_ack __attribute__ ((aligned (EDCL_XMODEM_BUF_ALIGN)));
 #endif // CONFIG_TARGET_1879VM8YA
 
 uint32_t flw_virt_to_dma(volatile void* ptr)
-{
-    return (uint32_t)ptr;
+{   
+    uint32_t _the_ptr = (uint32_t ) ptr; 
+    if (_the_ptr >= 0x100000 && _the_ptr < 0x200000) {
+        return (uint32_t) _the_ptr + 0x39000000 - 0x100000; 
+    }
+    return (uint32_t)_the_ptr;
 }
--- board/rcm/common/flw_buffers.h
+++ board/rcm/common/flw_buffers.h
@@ -3,19 +3,27 @@
 
 #include <linux/types.h>
 
-//#define CONFIRM_HANDSHAKE
-
-#define EDCL_XMODEM_BUF_LEN 4096
+#define EDCL_XMODEM_BUF_LEN (16384)
 #define EDCL_XMODEM_BUF_ALIGN 64
+//#define USE_DSP_MEM /* Doesn't work. Left here for later experiments only */
 
 #ifdef CONFIG_TARGET_1888TX018
+
+#ifndef USE_DSP_MEM
 extern volatile char blk_buf[];
 extern volatile char edcl_xmodem_buf0[];
 extern volatile char edcl_xmodem_buf1[];
 extern volatile char* edcl_xmodem_buf_sync;
-#ifdef CONFIRM_HANDSHAKE
-    extern volatile char* edcl_xmodem_sync_conf;
-#endif // CONFIRM_HANDSHAKE
+extern volatile char* edcl_xmodem_buf_sync_ack;
+#else
+extern volatile char* blk_buf;
+extern volatile char* edcl_xmodem_buf0;
+extern volatile char* edcl_xmodem_buf1;
+extern volatile char** p_edcl_xmodem_buf_sync;
+extern volatile char** p_edcl_xmodem_buf_sync_ack;
+#define edcl_xmodem_buf_sync (*p_edcl_xmodem_buf_sync)
+#define edcl_xmodem_buf_sync_ack (*p_edcl_xmodem_buf_sync_ack)
+#endif
 #endif // CONFIG_TARGET_1888TX018
 
 #ifdef CONFIG_TARGET_1888BM18
@@ -23,11 +31,9 @@ extern volatile char* blk_buf;
 extern volatile char* edcl_xmodem_buf0;
 extern volatile char* edcl_xmodem_buf1;
 extern volatile char** p_edcl_xmodem_buf_sync;
+extern volatile char** p_edcl_xmodem_buf_sync_ack;
 #define edcl_xmodem_buf_sync (*p_edcl_xmodem_buf_sync)
-#ifdef CONFIRM_HANDSHAKE
-    extern volatile char** p_edcl_xmodem_sync_conf;
-    #define edcl_xmodem_sync_conf (*p_edcl_xmodem_sync_conf)
-#endif // CONFIRM_HANDSHAKE
+#define edcl_xmodem_buf_sync_ack (*p_edcl_xmodem_buf_sync)
 #endif // CONFIG_TARGET_1888BM18
 
 #ifdef CONFIG_TARGET_1888BC048
@@ -37,9 +43,7 @@ extern volatile char blk_buf[];
 extern volatile char edcl_xmodem_buf0[];
 extern volatile char edcl_xmodem_buf1[];
 extern volatile char* edcl_xmodem_buf_sync;
-#ifdef CONFIRM_HANDSHAKE
-    extern volatile char* edcl_xmodem_sync_conf;
-#endif // CONFIRM_HANDSHAKE
+extern volatile char* edcl_xmodem_buf_sync_ack;
 #endif // CONFIG_TARGET_1888BC048
 
 #ifdef CONFIG_TARGET_1879VM8YA
@@ -49,9 +53,7 @@ extern volatile char blk_buf[];
 extern volatile char edcl_xmodem_buf0[];
 extern volatile char edcl_xmodem_buf1[];
 extern volatile char* edcl_xmodem_buf_sync;
-#ifdef CONFIRM_HANDSHAKE
-    extern volatile char* edcl_xmodem_sync_conf;
-#endif // CONFIRM_HANDSHAKE
+extern volatile char* edcl_xmodem_buf_sync_ack;
 #endif // CONFIG_TARGET_1879VM8YA
 
 uint32_t flw_virt_to_dma(volatile void* ptr);
